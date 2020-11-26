2e727b2ebc6a15fb803a9e80bf12dcfa37e47940
sidahmed <ahorob57@gmail.com>
2020-11-26 14:49:38 +0100

    add canvas

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tinyFaceDetectr.loadFromUri('/models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressNet.loadFromUri('/models'),
-])
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo)
 
 function startVideo() {
     navigator.getUserMedia(
